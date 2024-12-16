2578e56c303049084d593751f7dcf0edb087f93c
Roman Božik <web.kone@gmail.com>
2024-12-16 14:01:35 +0100

    Zdanlivo použiteľná verzia

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import clingo
 import re
 import sys
+import os
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QVBoxLayout, QLabel,
-    QListWidget, QPushButton, QWidget, QHBoxLayout, QStackedWidget
+    QListWidget, QPushButton, QWidget, QHBoxLayout, QStackedWidget, QGridLayout
 )
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
@@ -14,6 +15,8 @@ class Sokoban(QMainWindow):
         self.cesta_suboru = None
         self.mapa_data = []
         self.pociatocna_pozicia = None
+        self.mapa_solved = []
+        self.mapa_solved_odpad = {}
         self.init_ui()
 
     def mapa_do_asp(self):
@@ -43,6 +46,8 @@ class Sokoban(QMainWindow):
     def vykresli_mapu(self, riesenie):
         walls = set()
         crates = {}
+        crates_odpad = {}
+        move_odpad = {}
         storages = set()
         sokoban = None
         max_time = 0
@@ -50,6 +55,8 @@ class Sokoban(QMainWindow):
             match = re.match(r"(move|crate|push)\((\d+),", fakt)
             if match:
                 t = int(match.group(2))
+                crates_odpad[t] = []
+                move_odpad[t] = []
                 if t > max_time:
                     max_time = t
         for fakt in riesenie:
@@ -62,6 +69,7 @@ class Sokoban(QMainWindow):
                 match = re.match(r"crate\((\d+),(\d+),(\d+)\)", fakt)
                 if match:
                     t, x, y = map(int, match.groups())
+                    crates_odpad[t].append((x, y))
                     if t == max_time:
                         crates[(x, y)] = t
             elif fakt.startswith("storage"):
@@ -73,6 +81,7 @@ class Sokoban(QMainWindow):
                 match = re.match(r"move\((\d+),(\d+),(\d+)\)", fakt)
                 if match:
                     t, x, y = map(int, match.groups())
+                    move_odpad[t].append((x, y))
                     if t == max_time:
                         sokoban = (x, y)
         if not sokoban:
@@ -115,7 +124,14 @@ class Sokoban(QMainWindow):
             else:
                 mapa[y][x] = "S"
         for riadok in mapa:
-            print("".join(riadok))
+            pass
+            #print("".join(riadok))
+
+        self.mapa_solved = mapa
+        self.mapa_solved_odpad = {'walls':walls, 'crates':crates_odpad,
+                                  'storages':storages,'moves':move_odpad, 'max_time':max_time-1,
+                                  'max_width':max_width,
+                                  'max_height':max_height}
 
     def vyries(self,times=10):
         asp_reprezentacia = self.mapa_do_asp()
@@ -214,11 +230,11 @@ class Sokoban(QMainWindow):
         :- not theend(maxSteps).
         
         %Oddelenie jedla od odpadu| už netreba :)
-        #show move/3.
-        #show push/7.
-        #show storage/2.
-        #show wall/2.
-        #show crate/3.
+        %#show move/3.
+        %#show push/7.
+        %#show storage/2.
+        %#show wall/2.
+        %#show crate/3.
         """
         ctl = clingo.Control()
         ctl.add("base", [], asp_reprezentacia + "\n" + pravidla_pohybu)
@@ -228,10 +244,22 @@ class Sokoban(QMainWindow):
             riesenie.extend([str(symbol) for symbol in model.symbols(shown=True)])
         ctl.solve(on_model=model)
         self.vykresli_mapu(riesenie)
+        file_name = 'debug.txt'
+        with open(file_name, "w") as f:
+            f.write(f"% DEBUG pre mapu {self.cesta_suboru}\n\n")
+            f.write("% Pravidlá na základe vstupnej mapy\n")
+            f.write(asp_reprezentacia + "\n")
+            f.write("% Všeobecné pravidlá na riešenie\n")
+            f.write("\n".join(riadok.strip() for riadok in pravidla_pohybu.splitlines()) + "\n")
+
+            f.write("% Nájdený model\n")
+            for x in riesenie:
+                f.write(f"{x}\n")
+
         return riesenie
 
     def init_ui(self):
-        self.setWindowTitle("Sokoban Solver")
+        self.setWindowTitle("Výpočtová logika - Sokoban Solver ASP | Roman Božik")
         self.setGeometry(100, 100, 600, 400)
 
 
@@ -261,7 +289,7 @@ class Sokoban(QMainWindow):
         # Zoznam názvou máp
         self.mapovy_list = QListWidget()
         self.mapovy_list.setFont(QFont("Arial", 12))
-        self.mapovy_list.addItems(["map1.txt", "map2.txt", "map3.txt"])
+        self.mapovy_list.addItems(["map1.txt", "map4.txt", "map5.txt","map6.txt", "map8.txt"])
         self.mapovy_list.setStyleSheet("border: 1px solid #aaa;")
         m_menu_lajaut.addWidget(self.mapovy_list)
 
@@ -276,15 +304,45 @@ class Sokoban(QMainWindow):
         m_menu_widget.setLayout(m_menu_lajaut)
         return m_menu_widget
 
-    def vyrob_scenu_pre_mapu(self, map_name):
+    def vyrob_scenu_pre_mapu(self, mapa_cesta):
         mapa_w = QWidget()
         mapa_lay = QVBoxLayout()
 
-        map_label = QLabel(f"Riešim mapu: {map_name}")
+        map_label = QLabel(f"Riešim mapu: {mapa_cesta}")
         map_label.setAlignment(Qt.AlignCenter)
         map_label.setFont(QFont("Arial", 18, QFont.Bold))
         map_label.setStyleSheet("color: rgb(0, 0, 255);")
         mapa_lay.addWidget(map_label)
+
+        self.cesta_suboru = mapa_cesta
+        self.postupnost = []
+        for time in range(1, 1000):
+            temp = self.vyries(times=time)
+            if len(temp) != 0:
+                self.postupnost = temp
+                break
+
+        self.grid_layout = QGridLayout()
+        self.policka = {}
+
+        for x in range(self.mapa_solved_odpad["max_width"]):
+            for y in range(self.mapa_solved_odpad["max_height"]):
+                policko = QLabel(" ")
+                policko.setAlignment(Qt.AlignCenter)
+                policko.setFont(QFont("Courier", 16))
+                policko.setStyleSheet("border: 1px solid black; padding: 5px;")
+
+                if (x, y) in self.mapa_solved_odpad["walls"]:
+                    policko.setText("#")
+                    policko.setStyleSheet("background-color: gray; color: white;")
+                elif (x, y) in self.mapa_solved_odpad["storages"]:
+                    policko.setText("X")
+                    policko.setStyleSheet("background-color: blue; color: white;")
+
+                self.grid_layout.addWidget(policko, y, x)
+                self.policka[(x, y)] = policko
+
+        mapa_lay.addLayout(self.grid_layout)
 
 
         navigacia = QHBoxLayout()
@@ -314,24 +372,57 @@ class Sokoban(QMainWindow):
         mapa_lay.addWidget(menu_tlacidl)
 
         mapa_w.setLayout(mapa_lay)
+
+        self.aktualny_krok = 0
+        self.max_cas = self.mapa_solved_odpad["max_time"]
+        self.aktualizuj_mapu()
         return mapa_w
+
+    def aktualizuj_mapu(self):
+        try:
+            # Reset mapky
+            for (x, y), policko in self.policka.items():
+                policko.setText(" ")
+                policko.setStyleSheet("background-color: white; color: black; border: 1px solid black;")
+
+                if (x, y) in self.mapa_solved_odpad["walls"]:
+                    policko.setText("#")
+                    policko.setStyleSheet("background-color: gray; color: white;")
+                elif (x, y) in self.mapa_solved_odpad["storages"]:
+                    policko.setText("X")
+                    policko.setStyleSheet("background-color: blue; color: white;")
+
+            # Vykreslenie krabíc aktuálny čas
+            for x, y in self.mapa_solved_odpad["crates"].get(self.aktualny_krok, []):
+                policko = self.policka[(x, y)]
+                if policko.text() == "X":
+                    policko.setText("c")
+                    policko.setStyleSheet("background-color: green; color: white;")
+                else:
+                    policko.setText("C")
+                    policko.setStyleSheet("background-color: green; color: white;")
+
+            # Vykreslenie Sokobana
+            sokoban_pozicie = self.mapa_solved_odpad["moves"].get(self.aktualny_krok, [])
+            for x, y in sokoban_pozicie:
+                policko = self.policka[(x, y)]
+                if policko.text() == "X":
+                    policko.setText("s")
+                    policko.setStyleSheet("background-color: yellow; color: black;")
+                else:
+                    policko.setText("S")
+                    policko.setStyleSheet("background-color: yellow; color: black;")
+
+        except Exception as e:
+            print(f"Chyba pri aktualizácii mapky: {e}")
 
     def vyber_mapu(self):
         mapka_ako_item = self.mapovy_list.currentItem()
         if mapka_ako_item:
             mapka_meno = mapka_ako_item.text()
-            print(f"Riešim mapu: {mapka_meno}")
+            #print(f"Riešim mapu: {mapka_meno}")
 
-            # Simulacia krokov
-            self.vsetky_akcie_zoznam = [
-                "Krok 1: Sokoban sa pohol doprava",
-                "Krok 2: Sokoban sa pohol doprava",
-                "Krok 3: Sokoban posunul škatuľu",
-                "Krok 4: Sokoban sa pohol dole"
-            ]
-            self.aktualny_krok = 0
 
-            # Prepnutie do mapovej scény
             mapova_scenka = self.vyrob_scenu_pre_mapu(mapka_meno)
             self.zasobnik_widgetov.addWidget(mapova_scenka)
             self.zasobnik_widgetov.setCurrentWidget(mapova_scenka)
@@ -345,16 +436,16 @@ class Sokoban(QMainWindow):
         if self.aktualny_krok > 0:
             self.aktualny_krok -= 1
             self.aktualizuj_krok_label()
+            self.aktualizuj_mapu()
 
     def dalsi_krok(self):
-        if self.aktualny_krok < len(self.vsetky_akcie_zoznam) - 1:
+        if self.aktualny_krok < self.max_cas:
             self.aktualny_krok += 1
             self.aktualizuj_krok_label()
+            self.aktualizuj_mapu()
 
     def aktualizuj_krok_label(self):
-        if self.vsetky_akcie_zoznam:
-            self.label_pre_kroky.setText(f"Krok {self.aktualny_krok + 1} / {len(self.vsetky_akcie_zoznam)}")
-            print(self.vsetky_akcie_zoznam[self.aktualny_krok])
+        self.label_pre_kroky.setText(f"Krok {self.aktualny_krok} / {self.max_cas}")
 
     def nas5dohlmenu(self):
         self.zasobnik_widgetov.setCurrentWidget(self.main_menu)
